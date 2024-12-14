a6c782da34049c57f1b0b7533ae2c39f2126af58
pavansbhat <pavanagsbhat@gmail.com>
2024-12-14 16:32:26 +0530

    [feat-core] - Add pagination functionality to the table and andd few styling changes to the table component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@ import useFetcher from "./utils/useFetcher.js";
 import { CustomTable } from "./components/CustomTable.jsx";
 
 function App() {
-  const { data, error, loading, fetchData } = useFetcher(
+  const { data, error, loading } = useFetcher(
     "https://raw.githubusercontent.com/saaslabsco/frontend-assignment/refs/heads/master/frontend-assignment.json",
     { autoFetch: true },
   );
+  const [currentPage, setCurrentPage] = useState(1);
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <>
-      <CustomTable data={data} loading={loading} error={error} />
+      <CustomTable
+        data={data}
+        loading={loading}
+        error={error}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 }
--- src/components/CustomTable.jsx
+++ src/components/CustomTable.jsx
@@ -1,13 +1,31 @@
-import React, { useState, useEffect } from "react";
-import useFetcher from "../utils/useFetcher.js";
+import { useState } from "react";
 
-export const CustomTable = ({ data, loading, error }) => {
+import "./customTable.css";
+
+export const CustomTable = ({ data, loading, error, onPageChange }) => {
   const columns = ["Sl.No", "Percentage funded", "Amount pledged"];
+  const itemsPerPage = 5;
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(data && data.length / itemsPerPage);
+
+  const paginatedData =
+    data &&
+    data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  const handlePageChange = (newPage) => {
+    if (newPage > 0 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
 
   console.log(data, "DATA");
 
   return (
     <div className="custom-table-container">
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <table className="custom-table">
         <thead>
           <tr>
@@ -18,8 +36,35 @@ export const CustomTable = ({ data, loading, error }) => {
             ))}
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {paginatedData &&
+            paginatedData.map((item, itemIndex) => (
+              <tr className={"table-row"} key={itemIndex}>
+                <td>{item["s.no"]}</td>
+                <td>{item["amt.pledged"]}</td>
+                <td>{item["percentage.funded"]}%</td>
+              </tr>
+            ))}
+        </tbody>
       </table>
+
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
