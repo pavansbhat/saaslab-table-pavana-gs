3b8cc0b4c2308cafd90736618ca860267e6987c9
pavansbhat <pavanagsbhat@gmail.com>
2024-12-15 12:49:35 +0530

    [feat-core] - Add vercel speed insights capabilities to the app to check the core web vitals in vercel dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
-createRoot(document.getElementById('root')).render(
+injectSpeedInsights();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
